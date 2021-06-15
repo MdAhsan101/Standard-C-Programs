29b22ed09ab2dcd2873188eb28b85c944db5cdf7
MdAhsan <aliahsanali101@gmail.com>
2021-06-15 13:23:23 +0530

    AVL node deletion code included

--- BinarySearchTree_AVL.cpp
+++ BinarySearchTree_AVL.cpp
@@ -128,6 +128,58 @@ struct Node* RInsert(struct Node* p, int key)
     return p;
 }
 
+struct Node* Delete(struct Node* p, int key)
+{
+    if(p==NULL)
+        return NULL;
+
+    if(p->lchild==NULL && p->rchild==NULL)
+    {
+        if(root==p)
+        {
+            root=NULL;
+            return NULL;
+        }
+        delete p;
+        return NULL;
+    }
+
+    if(key<p->data)
+        p->lchild= Delete(p->lchild,key);
+    else if(key>p->data)
+        p->rchild= Delete(p->rchild,key);
+    else
+    {
+        if(Height(p->lchild)>Height(p->rchild))
+        {
+            struct Node* q= InorderPredecessor(p->lchild);
+            p->data=q->data;
+            p->lchild= Delete(p->lchild,q->data);
+        }
+        else
+        {
+            struct Node* q= InorderSuccessor(p->rchild);
+            p->data=q->data;
+            p->rchild= Delete(p->rchild,q->data);
+        }
+    }
+
+    if(BalanceFactor(p)==2 && BalanceFactor(p->lchild)==1)
+        return LL_Rotation(p); // Called L1 Rotation in case of deletion, Code is same as LL_Rotation
+    if(BalanceFactor(p)==2 && BalanceFactor(p->lchild)==-1)
+        return LR_Rotation(p); // Called L-1 Rotation
+    if(BalanceFactor(p)==2 && BalanceFactor(p->lchild)==0)
+        return LL_Rotation(p); // LO Rotation -> Can use either L1 or L-1, its our choice
+    if(BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==-1)
+        return RR_Rotation(p); // R-1 Rotation , Same as RR_Rotation code
+    if(BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==1)
+        return RL_Rotation(p); // R1 Rotation , Same as RL Rotation code
+    if(BalanceFactor(p)==-2 && BalanceFactor(p->lchild)==0)
+        return RR_Rotation(p); // R0 Rotation -> Can choise either of R1 or R-1 Rotation
+
+    return p;
+}
+
 void PreOrder(struct Node* p)
 {
     if(p)
@@ -187,6 +239,11 @@ int main()
     PostOrder(root);
     cout<<endl;
 
+    cout<<"Enter the node to be deleted: ";
+    cin>>key;
+    Delete(root,key);
+    cout<<endl;
+
     cout<<"Root node is "<<root->data<<endl;
     int tree_height=Height(root);
     cout<<"Height of AVL tree is "<< tree_height-1<<endl;
@@ -195,14 +252,18 @@ int main()
 /*
 Output:
 
-Enter the value for root node: 10
+Enter the value for root node: 33
 
-Enter the node value:
-20
+Enter the node value: 
+22
 Press any integer key to enter again: (Press -1 to exit) 
 0
 Enter the node value: 
-30
+77
+Press any integer key to enter again: (Press -1 to exit) 
+0
+Enter the node value: 
+15
 Press any integer key to enter again: (Press -1 to exit) 
 0
 Enter the node value: 
@@ -210,21 +271,30 @@ Enter the node value:
 Press any integer key to enter again: (Press -1 to exit) 
 0
 Enter the node value: 
-28
+55
 Press any integer key to enter again: (Press -1 to exit) 
 0
 Enter the node value: 
-27
+99
 Press any integer key to enter again: (Press -1 to exit) 
 0
 Enter the node value: 
-5
+11
+Press any integer key to enter again: (Press -1 to exit) 
+0
+Enter the node value: 
+35
+Press any integer key to enter again: (Press -1 to exit) 
+0
+Enter the node value: 
+66
 Press any integer key to enter again: (Press -1 to exit) 
 -1
+Preorder Seq: 33 22 15 11 25 77 55 35 66 99 
+Inorder Seq: 11 15 22 25 33 35 55 66 77 99
+Postorder Seq: 11 15 25 22 35 66 55 99 77 33 
+Enter the node to be deleted: 88
 
-Preorder Seq: 25 10 5 20 28 27 30
-Inorder Seq: 5 10 20 25 27 28 30
-Postorder Seq: 5 20 10 27 30 28 25
-Root node is 25
-Height of AVL tree is 2
+Root node is 33
+Height of AVL tree is 3
 */
